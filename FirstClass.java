d962f8e38dd7e8d439c924a6d9a362fb968abe77
binugsi <binumathai.gsi@gmail.com>
2018-01-08 15:21:15 +0530

    Cleared all comments
    Cleared all comments and added a single comment

--- FirstClass.java
+++ FirstClass.java
@@ -1,20 +1,6 @@
 
 public class FirstClass {
-//This is my first line in first java class
-	//This is second line
-
-	//This is for new branch branchonegsbinu
-	//aNOTHER CHANGE IN BRANCH ONE
-
-//This is in main branch se
-
-	//this is branch two
-
-	//another addtion afgter merger 
-
-
-	//This is addition to main branch
-	//Another change in main branch
+//cleared all comments and added this
 
 
 }
